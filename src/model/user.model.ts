036064bcddf92ca4fb532d80eead0b037c6b2b27
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-10 12:04:27 +0100

    re-wrote the user.model logic, refactored it what the teacher was using in his lecture

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,37 +1,58 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-export enum Gender {
-    male = 'male',
-    female = 'female',
-    undisclosed = 'undisclosed'
-}
-
-export interface Address extends Document {
-    street: string;
-    city: string;
-    postCode: string;
-}
-
-export interface IUser extends Document {
+export interface UserInput {
     email: string;
     firstName: string;
     lastName: string;
-    gender?: Gender;
-    address?: Address;
+    password: string;
 }
 
-const UserSchema: Schema = new Schema({
-    email: { type: String, required: true, unique: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    // Gets the Mongoose enum from the TypeScript enum
-    gender: { type: String, enum: Object.values(Gender) },
-    address: {
-        street: { type: String },
-        city: { type: String },
-        postCode: { type: String }
-    }
-});
+export interface UserDocument extends UserInput, Document{
+    fullName: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
 
-// Export the model and return your IUser interface
-export default mongoose.model<IUser>('User', UserSchema);
+const userSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    password: {type: String, required: true}
+})
+
+// indexes the email so that any search for a user using the email will be extremely fast
+userSchema.index({email: 1})
+
+//Virtual method for the fullName of the UserDocument and in essence the userSchema
+userSchema.virtual("fullName").get(function(this: UserDocument) {
+    return `${this.lastName} ${this.firstName}`
+})
+
+// hashing the password before we save the new user to our mongodb database
+userSchema.pre("save", async function (next) {
+    const user = this as UserDocument
+
+    // we only want to hash the password when it has not been modified or hashed
+    if (!user.isModified("password")) return next();
+
+    // use bcrypt to generate a new password
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hashSync(user.password, salt);
+
+    // update the current user password to the hashed one
+    user.password = hash;
+
+    // move on with the next function
+    return next();
+})
+
+// this method will be used to compare the received password vs the existing password when a user wants to login into their account
+userSchema.methods.comparePassword = async function (receivedPassword: string): Promise<boolean> {
+    const user = this as UserDocument
+    const check: boolean = await bcrypt.compare(user.password, receivedPassword)
+
+    if (!check) { return false }
+    return check
+}
